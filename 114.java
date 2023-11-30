c8fcbc1f32d416da9676debc0779b2cb19a1eb5f
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-11-30 23:56:03 +0530

    Create 114.java

---
+++ 114.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        
+        if(root==null)
+            return;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        
+        while(true){
+            
+            if(root.right != null)
+                stack.push(root.right);
+            
+            if(root.left==null){
+                if(stack.isEmpty())
+                    break;
+                root.right = stack.pop();
+            }
+            else{
+                root.right = root.left;
+                root.left = null;
+            }
+            root = root.right;
+        }
+        
+        return;
+    }
+}
