49259f38166380b93b11295891a3efd0a4be2a3b
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-12-05 22:55:29 +0530

    Create 71.java

---
+++ 71.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public String simplifyPath(String path) {
+        
+        String[] palabras = path.split("/");
+ 
+        
+        List<String> respuesta = new ArrayList<>();
+ 
+        
+        for (String palabra : palabras) {
+    
+            if (palabra.equals("..")) {
+                if (!respuesta.isEmpty())  
+                    respuesta.remove(respuesta.size() - 1);
+                continue;
+            }
+ 
+             
+            if (!palabra.equals("") && !palabra.equals(".")) {
+                respuesta.add(palabra);
+            }
+        }
+ 
+        //paso#4
+        return "/" + String.join("/", respuesta);
+    }
+}
