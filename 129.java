e54cfd576d4f6e520821d18827f47992e23edaa5
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-12-19 23:56:43 +0530

    Create 129.java

---
+++ 129.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result=0;
+    public void sum(TreeNode root, int num) {
+        if(root == null) return;
+        num = root.val + ( 10 * num);
+
+        if(root.left == null && root.right == null){
+            result += num;
+        }
+        sum(root.left, num);
+        sum(root.right, num);
+    }
+    public int sumNumbers(TreeNode root) {
+        sum(root, 0);
+        return result;
+    }
+}
