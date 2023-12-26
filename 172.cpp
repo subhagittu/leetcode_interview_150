2893e3b859cf6ec0aa63cdbacea229c4ebd7b9f5
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-12-26 23:59:39 +0530

    Create 172.cpp

---
+++ 172.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans=0;
+        int a=5;
+        while(a<=n){
+            ans+=n/a;
+            a*=5;
+        }
+        return ans;
+    }
+};
