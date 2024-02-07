7ec3ef6a4aeebcc6c0e8cf311f4165be0097498b
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-02-07 23:50:03 +0530

    Update 128.cpp

--- 128.cpp
+++ 128.cpp
@@ -1,16 +1,19 @@
 int a[100000];
 
-int init = [] {
+int init = [] 
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     ofstream out("user.out");
-    for (string s; getline(cin, s); out << '\n') {
+    for (string s; getline(cin, s); out << '\n') 
+    {
         if (s.length() == 2) {
             out << 0;
             continue;
         }
         int n = 0;
-        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
+        for (int _i = 1, _n = s.length(); _i < _n; ++_i) 
+        {
             bool _neg = false;
             if (s[_i] == '-') ++_i, _neg = true;
             int v = s[_i++] & 15;
@@ -20,7 +23,8 @@ int init = [] {
         }
         sort(a, a + n);
         int ans = 0;
-        for (int i = 0; i < n;) {
+        for (int i = 0; i < n;) 
+        {
             int i0 = i;
             for (++i; i < n && a[i - 1] + 1 >= a[i]; ++i);
             ans = max(ans, a[i - 1] - a[i0] + 1);
@@ -32,7 +36,8 @@ int init = [] {
     return 0;
 }();
 
-class Solution {
+class Solution
+{
 public:
     int longestConsecutive(vector<int>) { return 999; }
 };
