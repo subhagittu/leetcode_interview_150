f3dbf4b51cfb65e95cdcf19ca4bc2b33140a9a43
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-02-07 23:50:41 +0530

    Update 150.cpp

--- 150.cpp
+++ 150.cpp
@@ -1,18 +1,21 @@
 class Solution {
 
-    long resolves(int a, int b, char Operator){
+    long resolves(int a, int b, char Operator)
+{
         if(Operator == '+') return a + b;
         else if(Operator == '-') return a - b;
         else if(Operator == '*') return (long)a*b;
         return a/b;
     }
 public:
-    int evalRPN(vector<string>& tokens) {
+    int evalRPN(vector<string>& tokens) 
+{
         stack<long> Stack;
         int n = tokens.size();
         for(int i = 0; i < n; i++){
             // check if element is an operator
-            if(tokens[i].size() == 1 and tokens[i][0] < 48){
+            if(tokens[i].size() == 1 and tokens[i][0] < 48)
+            {
                 long integer2 = Stack.top();
                 Stack.pop();
                 long integer1 = Stack.top();
