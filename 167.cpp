ad390a3b6d9710a9aa973ae4d011b08a0b32bada
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-02-07 23:50:29 +0530

    Update 167.cpp

--- 167.cpp
+++ 167.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& numbers, int target) 
+{
         vector<int> ans;
         int s=0;
         int e= numbers.size()-1;
 
         while(s<e){
             int mid= s+(e-s)/2;
-                if( numbers[s]+numbers[e]==target){
+                if( numbers[s]+numbers[e]==target)
+                {
                     ans.push_back(s+1);
                     ans.push_back(e+1);
                     break;
                 }
-                else if( numbers[s]+numbers[e]>target){
+                else if( numbers[s]+numbers[e]>target)
+                {
                     e--;
 
                 }
