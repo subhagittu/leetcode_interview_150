c4aaac573aa3890f9e06f4acb21fc8b7e9cb55d6
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-03-11 23:38:28 +0530

    Update 209.java

--- 209.java
+++ 209.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int ans = Integer.MAX_VALUE;
-        int left=0;
+        int left=0; 
         int right=0;
         int sum=0;
         while(right<nums.length){
